3d8ed1b427f84805a1c219b34b47ca6e1ae7e97f
jacob ruleaux and Michelle Poterek <jacob@>
2017-08-14 13:46:25 -0700

    poop in html file to debug display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Pie Box!';
+  recipes: Recipe[] = [
+    new Recipe('Pancakes', 'flour, eggs, baking soda, water', 'mix it in a bowl and fry it up, bb!'),
+    new Recipe('eggs', 'eggs, oil, salt, pepper', 'mix it in a fish and fry it up, bb!'),
+    new Recipe('Pork lion', 'pork, lion, butter, brown sugar, sugggah', 'enter the wild. if you win you get to eat.'),
+  ];
+}
+
+export class Recipe {
+  constructor(public name: string, public ingredients: string, public directions: string) { }
 }
